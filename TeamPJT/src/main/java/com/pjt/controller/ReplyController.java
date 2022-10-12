3513867bc18f12b52f1a615014d320bbedfb2af7
hih5921 <110270714+hih5921@users.noreply.github.com>
2022-10-12 16:35:09 +0900

    DB설정 중 임시공유

--- TeamPJT/src/main/java/com/pjt/controller/ReplyController.java
+++ TeamPJT/src/main/java/com/pjt/controller/ReplyController.java
@@ -1,12 +1,9 @@
 package com.pjt.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +17,7 @@ import com.pjt.command.Criteria;
 import com.pjt.command.ReplyPageVO;
 import com.pjt.command.ReplyVO;
 import com.pjt.service.ReplyService;
+import com.pjt.service.ReplyServiceImpl;
 
 import lombok.AllArgsConstructor;
 
